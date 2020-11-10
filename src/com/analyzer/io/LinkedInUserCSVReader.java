6cd715ee2b51c099328506935418ec34ca2ea03c
Nick Scherer <nscherer30@gmail.com>
2020-11-09 18:35:20 -0800

    test method empty csv file works correctly

--- src/com/analyzer/io/LinkedInUserCSVReader.java
+++ src/com/analyzer/io/LinkedInUserCSVReader.java
@@ -7,7 +7,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 
 public class LinkedInUserCSVReader {
@@ -17,39 +19,51 @@ public class LinkedInUserCSVReader {
 		this.dataFilePath = Path.of(dataFilePath);
 	}
 
-	public List<Post> load() throws IOException {
+	public List<Post> load() throws IOException, ArrayIndexOutOfBoundsException {
+		Stream<String> readData = Files.lines(dataFilePath);
+		if(readData.findFirst().isEmpty())
+			return Collections.emptyList();
+
 		List<Post> result = new ArrayList<>();
 
 		Files.lines(dataFilePath).forEach(line -> {
+
 			// Separate all tokens on each line by a comma
 			String[] tokens = line.split(",");
 
-			// Read in csv tokens on each line, clean data of extra hidden characters
-			String postIdString = tokens[0].replaceAll("[^0-9]+", "");
-			String textBody = tokens[1].trim();
-			String hashTags = tokens[2].trim();
-			String likesString = tokens[3].replaceAll("[^0-9]+", "");
-			String commentsString = tokens[4].replaceAll("[^0-9]+", "");
-			String viewsString = tokens[5].replaceAll("[^0-9]+", "");
+			// Partial entry found
+			if(tokens.length != Post.class.getDeclaredFields().length){
+				// Handle accordingly
 
-			// Change raw string input to appropriate type
-			int postID = Integer.parseInt(postIdString);
-			// Further data cleaning for splitting up hashtags
-			String[] hashTagsDelimited = hashTags.split("#");
-			String[] hashTagsDelimitedNonNullValues = Arrays.stream(hashTagsDelimited)
-					.filter(s -> (s != null && s.length() > 0))
-					.toArray(String[]::new);
-			ArrayList<String> hashTagArrayList = new ArrayList<>();
-			hashTagArrayList.addAll(Arrays.asList(hashTagsDelimitedNonNullValues));
+			} else {
+				// Read in csv tokens on each line, clean data of extra hidden characters
+				String postIdString = tokens[0].replaceAll("[^0-9]+", "");
+				String textBody = tokens[1].trim();
+				String hashTags = tokens[2].trim();
+				String likesString = tokens[3].replaceAll("[^0-9]+", "");
+				String commentsString = tokens[4].replaceAll("[^0-9]+", "");
+				String viewsString = tokens[5].replaceAll("[^0-9]+", "");
+
+				// Change raw string input to appropriate type
+				int postID = Integer.parseInt(postIdString);
+				// Further data cleaning for splitting up hashtags
+				String[] hashTagsDelimited = hashTags.split("#");
+				String[] hashTagsDelimitedNonNullValues = Arrays.stream(hashTagsDelimited)
+						.filter(s -> (s != null && s.length() > 0))
+						.toArray(String[]::new);
+				ArrayList<String> hashTagArrayList = new ArrayList<>();
+				hashTagArrayList.addAll(Arrays.asList(hashTagsDelimitedNonNullValues));
 
 
-			int likes = Integer.parseInt(likesString);
-			int comments = Integer.parseInt(commentsString);
-			int views = Integer.parseInt(viewsString);
+				int likes = Integer.parseInt(likesString);
+				int comments = Integer.parseInt(commentsString);
+				int views = Integer.parseInt(viewsString);
 
-			// Create a new object with the converted values
-			result.add(new Post(postID, textBody, hashTagArrayList,
-					likes, comments, views));
+				// Create a new object with the converted values
+				result.add(new Post(postID, textBody, hashTagArrayList,
+						likes, comments, views));
+			}
+
 		});
 		return result;
 	}
--- test/com/analyzer/io/LinkedInUserCSVReaderTest.java
+++ test/com/analyzer/io/LinkedInUserCSVReaderTest.java
@@ -1,27 +1,57 @@
 package com.analyzer.io;
 
 import com.analyzer.data.Post;
+import org.junit.Before;
 import org.junit.Test;
-
+import static org.junit.Assert.*;
+import org.junit.Test;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LinkedInUserCSVReaderTest {
+	private String noEntryCSVPath;
+	private String oneEntryCSVPath;
+	private String twentyFiveEntriesCSVPath;
+
+	private LinkedInUserCSVReader readerWithNoEntries;
+	private LinkedInUserCSVReader readerWithOneEntry;
+	private LinkedInUserCSVReader readerWithTwentyFiveEntries;
+
+	private List<Post> noPosts;
+	private List<Post> onePost;
+	private List<Post> twentyFivePosts;
+
+	@Before
+	public void setUp() throws IOException {
+		noEntryCSVPath = "src/com/data/0-post-by-user.csv";
+		oneEntryCSVPath = "src/com/data/1-post-by-user.csv";
+		twentyFiveEntriesCSVPath = "src/com/data/25-post-by-user.csv";
+
+		readerWithNoEntries = new LinkedInUserCSVReader
+				(noEntryCSVPath);
+		readerWithOneEntry = new LinkedInUserCSVReader
+				(oneEntryCSVPath);
+		readerWithTwentyFiveEntries = new LinkedInUserCSVReader
+				(twentyFiveEntriesCSVPath);
+
+		noPosts = readerWithNoEntries.load();
+		onePost = readerWithOneEntry.load();
+		twentyFivePosts = readerWithTwentyFiveEntries.load();
+	}
 
 	@Test
-	public void testLoadReadsInStandardCSVDataOf25Posts() {
-		LinkedInUserCSVReader reader = new LinkedInUserCSVReader
-				("src/com/data/nick-sample-data-25-entries-no-commas.csv");
-		try {
-			List<Post> result = reader.load();
-			for (Post post : result) {
-				System.out.println(post);
-			}
+	public void testLoadReadsInStandardCSVDataOfNoPosts() {
+		List<Post> noPostList = new ArrayList<>();
+		assertEquals(noPostList, noPosts);
+	}
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	@Test
+	public void testLoadReadsInStandardCSVDataOfOnePost() {
+	}
 
+	@Test
+	public void testLoadReadsInStandardCSVDataOfTwentyFivePosts() {
 	}
 
 }
