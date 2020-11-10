efe642269bfe51711ed8ee6c69f88f89d5f09f63
Jason <jason55win@gmail.com>
2020-11-10 17:00:25 -0600

    Add Javadoc and break down code into few methods in HashtagSniffer

--- src/com/analyzer/trends/HashtagSniffer.java
+++ src/com/analyzer/trends/HashtagSniffer.java
@@ -1,12 +1,13 @@
 package com.analyzer.trends;
 
 import com.analyzer.data.Post;
-import com.analyzer.io.LinkedInUserCSVReader;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * HashtagSniffer, fist sorting list by number of views then by number of likes and lastly by number of comments.
@@ -14,18 +15,18 @@ import java.util.stream.Stream;
  */
 
 public class HashtagSniffer extends TrendFinder {
-    private int max = 0; // max number of hashtags appearances
+    private int max = 0; // max appearances of a hashtags
 
     public HashtagSniffer() throws IOException {
 
-    }
+    } // Constructor
 
     /**
-     * Returns a list of trend results according to users top posts.
-     * <p>
+     * Returns a list of Hashtag trend results according to users top posts.
      * This implementation specific method analyzes the top user posts.
      * It then will determine useful information for the user to know
-     * and returns a list of Strings as a result for future output.
+     * and returns a list of Hashtag Strings that appears most often
+     * as a result for future output.
      *
      * @param numOfResultsToFind the maximum number of results to find
      * @return the list of String results
@@ -33,16 +34,39 @@ public class HashtagSniffer extends TrendFinder {
      */
     @Override
     public List<String> findTrend(int numOfResultsToFind) throws IOException {
+        return getHashtagTrend(getHashtagList());
+    }
+
+    /**
+     * Returns a list of String Hashtags from all selected top posts
+     * and returns a list of all Hashtags within supplement posts
+     * as a input for getHashtagTrend().
+     *
+     * @return the list of all the String Hashtags from supplement posts as results
+     * @see List
+     */
+
+    public List<String> getHashtagList() {
         // sorting list, return a list of List<String> of hashtags for the top ${numOfResultsToFind} post/s.
         List<List<String>> list = getUserTopPostsFilteredByALimit().stream()
                 .map(Post::getListOfHashTags)
                 .collect(Collectors.toList());
-
         // flatten list into an list of string of all hashtags retrived.
         List<String> listOfHashtags = list.stream()
                 .flatMap(Collection::stream)
                 .collect(Collectors.toList());
 
+        return listOfHashtags;
+    }
+
+    /**
+     * Returns a list of Hashtags that appears most often from supplement posts
+     *
+     * @return the list of most popular Hashtags from supplement posts as results
+     * @see List
+     */
+
+    public List<String> getHashtagTrend(List<String> listOfHashtags) {
         // Insert listOfHashtags into Map<Integer, String>
         Map<Integer, String> mod = new HashMap<>();
         for (int i = 0; i < listOfHashtags.size(); i++) {
@@ -56,7 +80,6 @@ public class HashtagSniffer extends TrendFinder {
             mod.put(count, listOfHashtags.get(i));
         }
 
-
         // Find out the maximum appearances numbers.
         for (int count : mod.keySet()) {
             if (count > max) {
@@ -64,11 +87,11 @@ public class HashtagSniffer extends TrendFinder {
             }
         }
 
-        int finalMax = max;
         /*   // a sneak peak of the result of Map mode.
             for (Map.Entry<Integer,String> key: mod.entrySet()){
             System.out.println(key.getValue() +" appeared "+ key.getKey() +" time/s");
         }*/
+        int finalMax = max;
         List<String> modeOfHashtags = mod.entrySet().stream()
                 .filter(c -> c.getKey() == finalMax)
                 .map(Map.Entry::getValue)
@@ -77,3 +100,6 @@ public class HashtagSniffer extends TrendFinder {
         return modeOfHashtags;
     }
 }
+
+
+
--- test/com/analyzer/trends/HashtagSnifferTest.java
+++ test/com/analyzer/trends/HashtagSnifferTest.java
@@ -1,26 +1,22 @@
 package com.analyzer.trends;
 
-import static org.junit.Assert.*;
+import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 
-public class HashtagSnifferTest {
-    private HashtagSniffer hSniffer;
+public class HashtagSnifferTest extends TestCase {
+    public HashtagSniffer hSniffer;
 
     @Before
     public void setUp() throws IOException {
-            hSniffer = new HashtagSniffer();
+        hSniffer = new HashtagSniffer();
     }
 
     @Test
-    public void testHashtagSniffer() throws IOException {
-
+    public void testHashtagSniffer_resultShouldMatch() throws IOException {
         // expected result should match "[softwaredevelopment]"
         assertEquals("[softwaredevelopment]", hSniffer.findTrend(5).toString());
-
-        //System.out.println();
-        //System.out.println(hSniffer.findTrend(5));
     }
 }
