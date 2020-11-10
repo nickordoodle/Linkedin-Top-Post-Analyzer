ed05093b4b2c149281701bf8236f5d5f12e56faa
Nick Scherer <nscherer30@gmail.com>
2020-11-10 15:06:13 -0800

    filter method complete, lots of testing and docs to write still

---
+++ src/com/analyzer/dictionary/DictionaryHelper.java
@@ -0,0 +1,15 @@
+package com.analyzer.dictionary;
+
+public class DictionaryHelper {
+	public static boolean isPronoun(String keyword){
+		return false;
+	}
+
+	public static boolean isPreposition(String keyword){
+		return false;
+	}
+
+	public static boolean isConjunction(String keyword){
+		return false;
+	}
+}
--- src/com/analyzer/trends/TextContentKeyWordSniffer.java
+++ src/com/analyzer/trends/TextContentKeyWordSniffer.java
@@ -1,6 +1,7 @@
 package com.analyzer.trends;
 
 import com.analyzer.data.Post;
+import com.analyzer.dictionary.DictionaryHelper;
 
 import java.io.IOException;
 import java.util.*;
@@ -10,7 +11,7 @@ public class TextContentKeyWordSniffer extends TrendFinder {
 	private List<Post> topUsersPosts;
 
 	public TextContentKeyWordSniffer() throws IOException {
-		this.topUsersPosts = filterTextPosts(getUserTopPostsFilteredByALimit());
+		this.topUsersPosts = getUserTopPostsFilteredByALimit();
 	}
 
 	/**
@@ -18,36 +19,21 @@ public class TextContentKeyWordSniffer extends TrendFinder {
 	 * <p>
 	 * Anaylzes the top posts by cross referencing the most common words used.
 	 * These will be referred to as 'keywords' and the assumption is that these
-	 * keywords positively increased their posts reach according to LinkedIns algorithm.
+	 * keywords will positively increase a post's reach according to LinkedIns algorithm.
 	 *
 	 * @param numOfResultsToFind the maximum number of results to find
-	 * @return the list of String results
+	 * @return the list of String results e.g. {"engineers", "technology", "learn"}
 	 * @see List
 	 */
 	@Override
 	public List<String> findTrend(int numOfResultsToFind) {
-
-		// Whats a keyword?  Most common words used throughout posts
-		// only compare keywords to other posts, not against its own post
-		// Get occurrences of every word in a unique post
-		// ie. "development" occurs in Post 1, Post 3, Post 10.
-		// so Map<String, occurences> is "development", 3
-		// Continue to search and add for keywords until threshold of result is reached
-// sorting list, return a list of List<String> of hashtags for the top ${numOfResultsToFind} post/s.
-
 		// Map all text content into its own list for comparing
 		List<String> topPostTextContentList = this.topUsersPosts.stream()
 				.map(Post::getMainTextContent)
 				.collect(Collectors.toList());
 
 		List<String> listOfKeyWords = new ArrayList<>();
-
-		for (String textContent: topPostTextContentList){
-			List<String> filteredWords = Arrays.asList(textContent.split("\\W"));
-			// Remove any possible empty values included
-			filteredWords.removeIf(String::isEmpty);
-			listOfKeyWords.addAll(filteredWords);
-		}
+		listOfKeyWords = filterTextPosts(topPostTextContentList);
 
 		Map<String, Integer> keywordOccurrences = new HashMap<>();
 		listOfKeyWords.forEach(keyword -> {
@@ -70,18 +56,37 @@ public class TextContentKeyWordSniffer extends TrendFinder {
 
 
 	/**
-	 * Returns a list of Posts with irrelvant words filtered out of their text contents
+	 * Returns a list of Posts with irrelevant words filtered out of their text contents
 	 * <p>
 	 * Inspects the text content of each post. Removes irrelevant words and grammar
 	 * such as: pronouns, prepositions and conjunctions.  Meaningful text content
 	 * in this case consists of nouns, verbs, adjectives and adverbs.
 	 *
-	 * @param userPosts the list of Posts to filter
+	 * @param userTextContentFromPosts the list of Posts to filter
 	 * @return the list of filtered Posts
 	 * @see List
 	 */
-	private List<Post> filterTextPosts(List<Post> userPosts) {
+	private List<String> filterTextPosts(List<String> userTextContentFromPosts) {
 		//TODO implement, should remove prepositions, pronouncs, conjunctions etc.
-		return null;
+		List<String> filteredTextContentFromPosts = new ArrayList<>();
+
+		// Filter to only words, remove any empties and remove pronouns, prepositions and conjunctions
+		for (String textContent: userTextContentFromPosts){
+			List<String> filteredWords = Arrays.asList(textContent.split("\\W"));
+			// Remove any possible empty values included
+			filteredWords.removeIf(String::isEmpty);
+			filteredWords.removeIf(keyword -> {
+				if(DictionaryHelper.isPronoun(keyword)
+						|| DictionaryHelper.isPreposition(keyword)
+						|| DictionaryHelper.isConjunction(keyword)){
+					return true;
+				}
+				return false;
+			});
+
+			filteredTextContentFromPosts.addAll(filteredWords);
+		}
+
+		return filteredTextContentFromPosts;
 	}
 }
