dfbb1fb5290d6992b76382a7145d6c9894d8acda
Nick Scherer <nscherer30@gmail.com>
2020-11-10 10:02:24 -0800

    created text content testing class

---
+++ test/com/analyzer/trends/TextContentKeyWordSnifferTest.java
@@ -0,0 +1,17 @@
+package com.analyzer.trends;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TextContentKeyWordSnifferTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void findTrend() {
+	}
+}
