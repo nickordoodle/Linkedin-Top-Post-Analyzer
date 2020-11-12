9b8c4b5aab90745a809815dff49bb23022b79d8e
Jason <jason55win@gmail.com>
2020-11-11 20:18:56 -0600

    add bannerLoader and cleanScreen method in IOHandler

--- src/com/analyzer/io/IOHandler.java
+++ src/com/analyzer/io/IOHandler.java
@@ -5,6 +5,8 @@ import com.analyzer.trends.TextContentKeyWordSniffer;
 import com.apps.util.Prompter;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
@@ -18,14 +20,17 @@ public class IOHandler {
     private static TextContentKeyWordSniffer keyWordSniffer;
     private static boolean isExit = false;
     private static String csvFilePatch;
-    private static String followUpMsg = "Please enter 'Y' to continue to analyze another document or 'N' to exit" + "\n" + ">>";
+    private static String followUpMsg = "Incorporate above Hashtag and Keyword could increase the popularity of your next post " +
+            "\n" + "Please enter 'Y' to continue to analyze another document or 'N' to exit" + "\n" + ">>";
     private static String welcomeMsg = "Please provide the document in csv format" + "\n" + ">>";
     private static final String goodbyeMsg = "Thank you for choosing Lambda Stream Technology";
 
     // Constructor
-    public IOHandler() {}
+    public IOHandler() {
+    }
 
     // Methods
+
     /**
      * Main method in Writer Class, act as a wrapper inside IOHandler.
      * <p>
@@ -34,15 +39,16 @@ public class IOHandler {
      * to accomplish all IO tasks.
      * for details of each method, consult each individual method
      * API documents.
-     *
      */
     public void write() throws IOException, InterruptedException {
         do {
+            bannerLoader();
             read();
             result();
         }
         while (isExit());
     }
+
     /**
      * process user entry and invoke load() method in LinkedInUserCSVReader.
      * <p>
@@ -50,12 +56,16 @@ public class IOHandler {
      * to capture data from csv document for further process.
      * for details of each method, consult each individual method
      * API documents.
-     *
      */
-    private void read() throws IOException {
+    private void read() throws IOException, InterruptedException {
         csvFilePatch = prompter.prompt(welcomeMsg);
-        reader = new LinkedInUserCSVReader(csvFilePatch);
-        reader.load();
+        if (!csvFilePatch.equals("")) {
+            reader = new LinkedInUserCSVReader(csvFilePatch);
+            reader.load();
+        } else {
+            cleanScreen();
+            write();
+        }
     }
 
     /**
@@ -65,7 +75,6 @@ public class IOHandler {
      * to prepare readable result of analysis.
      * for details of each method, consult each individual method
      * API documents.
-     *
      */
     private void result() throws IOException {
         System.out.println("The popular Hashtag(s) is/are: " + regexCleaner(hashtagTrend(5)) + "\n"
@@ -79,18 +88,15 @@ public class IOHandler {
      * the program.
      * for details of each method, consult each individual method
      * API documents.
-     *
      */
     private boolean isExit() throws InterruptedException {
         String entry = prompter.prompt(followUpMsg);
-        if (entry.toUpperCase().equals("Y")) {
-            isExit = true;
-        } else {
+        if (!entry.toUpperCase().equals("Y")) {
             System.out.println(goodbyeMsg);
             TimeUnit.SECONDS.sleep(3);
             System.exit(1);
         }
-        return isExit;
+        return true;
     }
 
     /**
@@ -99,7 +105,6 @@ public class IOHandler {
      * This implementation invoke method in HashtagSniffer.
      * for details of each method, consult each individual method
      * API documents.
-     *
      */
     public List<String> hashtagTrend(int numOfResultsToFind) throws IOException {
         hashtagSniffer = new HashtagSniffer();
@@ -112,24 +117,54 @@ public class IOHandler {
      * This implementation invoke method in TextContentKeyWordSniffer.
      * for details of each method, consult each individual method
      * API documents.
-     *
      */
     public List<String> keywordTrend(int numOfResultsToFind) throws IOException {
         keyWordSniffer = new TextContentKeyWordSniffer();
         return keyWordSniffer.findTrend(numOfResultsToFind);
     }
+
     /**
      * remove open and close bracket from returned string.
      * <p>
      * This implementation invoke replaceAll method with regex.
      * for details of each method, consult each individual method
      * API documents.
-     *
      */
     private String regexCleaner(List<String> popResult) {
         return popResult.toString().replaceAll("\\[", "").replaceAll("\\]", "");
     }
 
+    /**
+     * load and print banner from banner.txt file.
+     * <p>
+     * This implementation read from external txt file for banner.
+     * for details of each method, consult each individual method
+     * API documents.
+     */
+    private void bannerLoader() throws IOException {
+        try {
+            String banner = Files.readString(Path.of("src", "com", "data", "banner.txt"));
+            System.out.println(banner);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
+    /**
+     * Clear screen if user decide to continue or click enter without valid path.
+     * <p>
+     * This implementation issue a system command base on system type.
+     * for details of each method, consult each individual method
+     * API documents.
+     */
+    public static void cleanScreen() throws IOException {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")){
+            Runtime.getRuntime().exec("cls");
+        }
+        else{
+            Runtime.getRuntime().exec("clear");
+        }
+    }
 
 }
