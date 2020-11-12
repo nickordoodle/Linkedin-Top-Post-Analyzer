a2ffa2e20c17292e1fe1497d1b9dde8ebe53d6d1
Nick Scherer <nscherer30@gmail.com>
2020-11-11 19:37:17 -0800

    added generated javadocs under docs folder.  will update generation before we submit

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.analyzer.client"},{"l":"com.analyzer.data"},{"l":"com.analyzer.helper"},{"l":"com.analyzer.io"},{"l":"com.analyzer.sniffer"}]
