35c2ee03a393964d055f58ea504df8a4b533acf5
Nick Scherer <nscherer30@gmail.com>
2020-11-11 17:20:44 -0800

    pulled IOHandler

--- src/com/analyzer/io/IOHandler.java
+++ src/com/analyzer/io/IOHandler.java
@@ -10,124 +10,121 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class IOHandler {
-    // Field
-    private static String popResult;
-    private static Prompter prompter = new Prompter(new Scanner(System.in));
-    private static LinkedInUserCSVReader reader;
-    private static HashtagSniffer hashtagSniffer;
-    private static TextContentKeyWordSniffer keyWordSniffer;
-    private static boolean isExit = false;
-    private static String csvFilePatch;
-    private static String followUpMsg = "Please enter 'Y' to continue to analyze another document or 'N' to exit" + "\n" + ">>";
-    private static String welcomeMsg = "Please provide the document in csv format" + "\n" + ">>";
-    private static final String goodbyeMsg = "Thank you for choosing Lambda Stream Technology";
+	// Field
+	private static String popResult;
+	private static Prompter prompter = new Prompter(new Scanner(System.in));
+	private static LinkedInUserCSVReader reader;
+	private static HashtagSniffer hashtagSniffer;
+	private static TextContentKeyWordSniffer keyWordSniffer;
+	private static boolean isExit = false;
+	private static String csvFilePatch;
+	private static String followUpMsg = "Please enter 'Y' to continue to analyze another document or 'N' to exit" + "\n" + ">>";
+	private static String welcomeMsg = "Please provide the document in csv format" + "\n" + ">>";
+	private static final String goodbyeMsg = "Thank you for choosing Lambda Stream Technology";
 
-    // Constructor
-    public IOHandler() {}
+	// Constructor
+	public IOHandler() {
+	}
 
-    // Methods
-    /**
-     * Main method in Writer Class, act as a wrapper inside IOHandler.
-     * <p>
-     * This implementation utilize private methods read(), result(),
-     * isExit(), hashtagTrend(), keywordTrend() and regexCleaner
-     * to accomplish all IO tasks.
-     * for details of each method, consult each individual method
-     * API documents.
-     *
-     */
-    public void write() throws IOException, InterruptedException {
-        do {
-            read();
-            result();
-        }
-        while (isExit());
-    }
-    /**
-     * process user entry and invoke load() method in LinkedInUserCSVReader.
-     * <p>
-     * This implementation utilize load() method in LinkedInUserCSVReader
-     * to capture data from csv document for further process.
-     * for details of each method, consult each individual method
-     * API documents.
-     *
-     */
-    private void read() throws IOException {
-        csvFilePatch = prompter.prompt(welcomeMsg);
-        reader = new LinkedInUserCSVReader(csvFilePatch);
-        reader.load();
-    }
+	// Methods
 
-    /**
-     * output analysis result.
-     * <p>
-     * This implementation utilize hashtagTrend(), keywordTrend() methods
-     * to prepare readable result of analysis.
-     * for details of each method, consult each individual method
-     * API documents.
-     *
-     */
-    private void result() throws IOException {
-        System.out.println("The popular Hashtag(s) is/are: " + regexCleaner(hashtagTrend(5)) + "\n"
-                + "The popular Keyword(s) is/are: " + regexCleaner(keywordTrend(5)));
-    }
+	/**
+	 * Main method in Writer Class, act as a wrapper inside IOHandler.
+	 * <p>
+	 * This implementation utilize private methods read(), result(),
+	 * isExit(), hashtagTrend(), keywordTrend() and regexCleaner
+	 * to accomplish all IO tasks.
+	 * for details of each method, consult each individual method
+	 * API documents.
+	 */
+	public void write() throws IOException, InterruptedException {
+		do {
+			read();
+			result();
+		}
+		while (isExit());
+	}
 
-    /**
-     * provide flag for program to indicate program flow.
-     * <p>
-     * This implementation return boolean value to continue or exit
-     * the program.
-     * for details of each method, consult each individual method
-     * API documents.
-     *
-     */
-    private boolean isExit() throws InterruptedException {
-        String entry = prompter.prompt(followUpMsg);
-        if (entry.toUpperCase().equals("Y")) {
-            isExit = true;
-        } else {
-            System.out.println(goodbyeMsg);
-            TimeUnit.SECONDS.sleep(3);
-            System.exit(1);
-        }
-        return isExit;
-    }
+	/**
+	 * process user entry and invoke load() method in LinkedInUserCSVReader.
+	 * <p>
+	 * This implementation utilize load() method in LinkedInUserCSVReader
+	 * to capture data from csv document for further process.
+	 * for details of each method, consult each individual method
+	 * API documents.
+	 */
+	private void read() throws IOException {
+		csvFilePatch = prompter.prompt(welcomeMsg);
+		reader = new LinkedInUserCSVReader(csvFilePatch);
+		reader.load();
+	}
 
-    /**
-     * return list of popular hashtags.
-     * <p>
-     * This implementation invoke method in HashtagSniffer.
-     * for details of each method, consult each individual method
-     * API documents.
-     *
-     */
-    public List<String> hashtagTrend(int numOfResultsToFind) throws IOException {
-        hashtagSniffer = new HashtagSniffer();
-        return hashtagSniffer.findTrend(numOfResultsToFind);
-    }
+	/**
+	 * output analysis result.
+	 * <p>
+	 * This implementation utilize hashtagTrend(), keywordTrend() methods
+	 * to prepare readable result of analysis.
+	 * for details of each method, consult each individual method
+	 * API documents.
+	 */
+	private void result() throws IOException {
+		System.out.println("The popular Hashtag(s) is/are: " + regexCleaner(hashtagTrend(5)) + "\n"
+				+ "The popular Keyword(s) is/are: " + regexCleaner(keywordTrend(5)));
+	}
 
-    /**
-     * return list of popular keywords.
-     * <p>
-     * This implementation invoke method in TextContentKeyWordSniffer.
-     * for details of each method, consult each individual method
-     * API documents.
-     *
-     */
-    public List<String> keywordTrend(int numOfResultsToFind) throws IOException {
-        keyWordSniffer = new TextContentKeyWordSniffer();
-        return keyWordSniffer.findTrend(numOfResultsToFind);
-    }
-    /**
-     * remove open and close bracket from returned string.
-     * <p>
-     * This implementation invoke replaceAll method with regex.
-     * for details of each method, consult each individual method
-     * API documents.
-     *
-     */
-    private String regexCleaner(List<String> popResult) {
-        return popResult.toString().replaceAll("\\[", "").replaceAll("\\]", "");
-    }
+	/**
+	 * provide flag for program to indicate program flow.
+	 * <p>
+	 * This implementation return boolean value to continue or exit
+	 * the program.
+	 * for details of each method, consult each individual method
+	 * API documents.
+	 */
+	private boolean isExit() throws InterruptedException {
+		String entry = prompter.prompt(followUpMsg);
+		if (entry.toUpperCase().equals("Y")) {
+			isExit = true;
+		} else {
+			System.out.println(goodbyeMsg);
+			TimeUnit.SECONDS.sleep(3);
+			System.exit(1);
+		}
+		return isExit;
+	}
+
+	/**
+	 * return list of popular hashtags.
+	 * <p>
+	 * This implementation invoke method in HashtagSniffer.
+	 * for details of each method, consult each individual method
+	 * API documents.
+	 */
+	public List<String> hashtagTrend(int numOfResultsToFind) throws IOException {
+		hashtagSniffer = new HashtagSniffer();
+		return hashtagSniffer.findTrend(numOfResultsToFind);
+	}
+
+	/**
+	 * return list of popular keywords.
+	 * <p>
+	 * This implementation invoke method in TextContentKeyWordSniffer.
+	 * for details of each method, consult each individual method
+	 * API documents.
+	 */
+	public List<String> keywordTrend(int numOfResultsToFind) throws IOException {
+		keyWordSniffer = new TextContentKeyWordSniffer();
+		return keyWordSniffer.findTrend(numOfResultsToFind);
+	}
+
+	/**
+	 * remove open and close bracket from returned string.
+	 * <p>
+	 * This implementation invoke replaceAll method with regex.
+	 * for details of each method, consult each individual method
+	 * API documents.
+	 */
+	private String regexCleaner(List<String> popResult) {
+		return popResult.toString().replaceAll("\\[", "").replaceAll("\\]", "");
+	}
 
 }
